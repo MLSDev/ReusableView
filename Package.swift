7bdeb57543fa4ec1b5c9a19cfe29967fbb41b05f
Rostyslav Dovhaliuk <rdovhaliuk@outlook.com>
2019-12-16 10:35:23 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-    name: "ReusableView"
+    name: "ReusableView",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10)
+    ],
+    products: [
+        .library(name: "ReusableView", targets: ["ReusableView"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
+    ],
+    targets: [
+        .target(name: "ReusableView", dependencies: ["RxSwift"])
+    ]
 )
